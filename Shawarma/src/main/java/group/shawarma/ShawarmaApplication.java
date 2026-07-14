0dcbe59683b0eac13f95b4212766069905845e31
WithVery <mojaddos@gmail.com>
2026-07-14 17:52:24 +0400

    Lost file added.

---
+++ Shawarma/src/main/java/group/shawarma/ShawarmaApplication.java
@@ -0,0 +1,13 @@
+package group.shawarma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShawarmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShawarmaApplication.class, args);
+	}
+
+}
